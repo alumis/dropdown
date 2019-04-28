1cc403713edbd23f70d268661dab3f24938ecb08
andrewsadduq <andrewnielsen89@gmail.com>
2019-04-28 23:19:30 +0200

    fixed stupid mistake

---
+++ index.ts
@@ -0,0 +1,11 @@
+
+export * from './src/AlumisButtonAttributes';
+export * from './src/AlumisDropdown';
+export * from './src/AlumisDropdownAttributes';
+export * from './src/AlumisDropdownFadeAnimator';
+export * from './src/AlumisDropdownItem';
+export * from './src/AlumisOrderedDropdown';
+export * from './src/AlumisUnorderedDropdown';
+export * from './src/bindDropdown';
+export * from './src/HTMLListElement';
+export * from './src/IAlumisDropdownAnimator';
